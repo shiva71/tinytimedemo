eaa480a31dce80e69c5453797358a74fd4b01281
shiva <shivashandilya1@gmail.com>
2020-07-13 12:21:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'jquery';
+import 'popper.js';
+import 'bootstrap/dist/js/bootstrap';
+import Navbar from './components/layout/Navbar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <nav className='navbar navbar-expand-lg navbar-light bg-white shadow-sm p-3 mb-5 bg-white rounded'>
+      <Link className='navbar-brand mb-0 h1' style={{ color: '#4c0080' }}>
+        <i className={icon} /> {title}
+      </Link>
+      <button
+        className='navbar-toggler'
+        type='button'
+        data-toggle='collapse'
+        data-target='#navbarNav'
+        aria-controls='navbarNav'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
+      >
+        <span className='navbar-toggler-icon'></span>
+      </button>
+      <div className='collapse navbar-collapse' id='navbarNav'>
+        <ul className='navbar-nav'>
+          <li className='nav-item active'>
+            <Link className='nav-link' to='#'>
+              Dashboard <span className='sr-only'>(current)</span>
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link className='nav-link' to='#'>
+              Project
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link className='nav-link' to='#'>
+              Settings
+            </Link>
+          </li>
+        </ul>
+        <ul className='nav navbar-nav ml-auto'>
+          <i className='far fa-user-circle' />
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: 'TinyTime',
+  icon: 'fas fa-stopwatch',
+};
+
+export default Navbar;
